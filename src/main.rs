e4c6e60728481e3f20f863dfa57acbc741cbdb95
Arpith Siromoney <arpith@feedreader.co>
2018-05-01 13:14:09 -0400

    use url from args, print address

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
+extern crate itertools;
+use itertools::join;
 use std::net::UdpSocket;
-use std::str;
+use std::env;
 
 fn main() {
-    let buf = &[0xAA, 0xAA, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x07, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d,
-                0x00, 0x00, 0x01, 0x00, 0x01];
-    println!("going to bind to address");
+    let args: Vec<String> = env::args().collect();
+    let default_name: String = "example.com".to_string();
+    let split_name = args.last().unwrap_or(&default_name).split(".");
+    let mut buf = vec![0xAA, 0xAA, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+    for part in split_name {
+        buf.push(part.len() as u8);
+        buf.extend_from_slice(part.as_bytes());
+    }
+    buf.append(&mut vec![0x00, 0x00, 0x01, 0x00, 0x01]);
     let socket = UdpSocket::bind("0.0.0.0:12345").expect("couldn't bind to address");
-    println!("going to send data");
-    socket.send_to(buf, "8.8.8.8:53").expect("couldn't send data");
-    println!("going to receive");
-    let mut buf = [0; 10];
-    let (amt, src) = socket.recv_from(&mut buf).expect("Didn't receive data");
-    println!("going to print");
-
-    let s = match str::from_utf8(&buf) {
-        Ok(v) => v,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    };
-    println!("result: {}", s);
-    println!("amt: {}", amt);
-    println!("src: {}", src);
+    socket.send_to(&buf, "8.8.8.8:53").expect("couldn't send data");
+    let mut buf = [0; 64];
+    let (amt, _src) = socket.recv_from(&mut buf).expect("Didn't receive data");
+    println!("{}", join(&buf[amt-4 .. amt], "."));
 }
